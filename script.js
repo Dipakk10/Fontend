7121819b8db481972aef2b6232acb3b0679080dc
Dipak <dipak8581987732@gmail.com>
2023-03-16 13:41:43 +0530

    update

---
+++ script.js
@@ -0,0 +1,43 @@
+function logfun() {
+    var desc = document.getElementById("des"),
+        box = document.getElementById('bx');
+
+    desc.style.display = 'none';
+    box.style.display = 'block';
+}
+
+function regfun() {
+    var desc = document.getElementById("des"),
+        box = document.getElementById('bx2');
+
+    desc.style.display = 'none';
+    box.style.display = 'block';
+}
+
+function AboutFun() {
+    var homeButton = document.getElementById("Aboutbutton"),
+        homeId = document.getElementById("AboutId");
+
+    homeId.style.display = 'block';    
+}
+
+function ServiceFun() {
+    var homeButton = document.getElementById("Servicebutton"),
+        homeId = document.getElementById("ServiceId");
+
+    homeId.style.display = 'block';    
+}
+
+function ContactFun() {
+    var homeButton = document.getElementById("Contactbutton"),
+        homeId = document.getElementById("ContactId");
+
+    homeId.style.display = 'block';    
+}
+
+function indexFun() {
+    var homeButton = document.getElementById("indexbutton"),
+        homeId = document.getElementById("indexId");
+
+    homeId.style.display = 'block';    
+}
